9f70a339620cc38ead77777333abdb0ead5359e1
kevin-llps <llopis.kevin@outlook.fr>
2024-08-21 23:37:15 +0200

    feat: Create EkwCustomerBillingApplication

---
+++ src/main/java/fr/kevin/llps/ekw/customer/billing/EkwCustomerBillingApplication.java
@@ -0,0 +1,13 @@
+package fr.kevin.llps.ekw.customer.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwCustomerBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkwCustomerBillingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/kevin/llps/ekw/customer/billing/EkwCustomerBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kevin.llps.ekw.customer.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkwCustomerBillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
